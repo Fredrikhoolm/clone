333b059d5b3e3501ef41bebe1cb3c69f8f0b009a
hkonn <hkonnhaagaas@gmail.com>
2020-10-26 20:52:39 +0100

    added some lines in projectDao

--- src/main/java/no/kristiania/Project/ProjectDao.java
+++ src/main/java/no/kristiania/Project/ProjectDao.java
@@ -1,5 +1,21 @@
 package no.kristiania.Project;
 
-public class ProjectDao {
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class ProjectDao extends AbstractDao {
+
+    public ProjectDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    public void insert(Project project) throws SQLException {
+
+    }
+
+    @Override
+    protected Project mapRow(ResultSet rs) throws SQLException {
+        return null;
+    }
 }
