8161a9cf8bb4e5edeeb33b3f31de7bbaf63a187b
crlsocro <christian.r.lie@hotmail.no>
2020-10-26 19:16:22 +0100

    gg

---
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -0,0 +1,110 @@
+package no.kristiania.http;
+
+import no.kristiania.Project.Member;
+import no.kristiania.Project.MemberDao;
+import org.flywaydb.core.Flyway;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
+import java.util.Date;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HttpServerTest {
+
+    private JdbcDataSource dataSource;
+
+    @BeforeEach
+    void setUp() {
+        dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+
+        Flyway.configure().dataSource(dataSource).load().migrate();
+    }
+
+    @Test
+    void shouldReturnSuccessfulStatusCode() throws IOException {
+        new HttpServer(10001, dataSource);
+        HttpClient client = new HttpClient("localhost", 10001, "/echo");
+        assertEquals(200, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnUnsuccessfulStatusCode() throws IOException {
+        new HttpServer(10002, dataSource);
+        HttpClient client = new HttpClient("localhost", 10002, "/echo?status=404");
+        assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnContentLength() throws IOException {
+        new HttpServer(10003, dataSource);
+        HttpClient client = new HttpClient("localhost", 10003, "/echo?body=HelloWorld");
+        assertEquals("10", client.getResponseHeader("Content-Length"));
+    }
+
+    @Test
+    void shouldReturnResponseBody() throws IOException {
+        new HttpServer(10004, dataSource);
+        HttpClient client = new HttpClient("localhost", 10004, "/echo?body=HelloWorld");
+        assertEquals("HelloWorld", client.getResponseBody());
+    }
+
+    @Test
+    void shouldReturnFileFromDisk() throws IOException {
+        HttpServer server = new HttpServer(10005, dataSource);
+        File contentRoot = new File("target/test-classes");
+        String fileContent = "Hello World " + new Date();
+        Files.writeString(new File(contentRoot, "test.txt").toPath(), fileContent);
+        HttpClient client = new HttpClient("localhost", 10005, "/test.txt");
+        assertEquals(fileContent, client.getResponseBody());
+        assertEquals("text/plain", client.getResponseHeader("Content-Type"));
+    }
+
+    @Test
+    void shouldReturnCorrectContentType() throws IOException {
+        HttpServer server = new HttpServer(10006, dataSource);
+        File contentRoot = new File("target/test-classes");
+        Files.writeString(new File(contentRoot, "index.html").toPath(), "<h2>Hello World</h2>");
+        HttpClient client = new HttpClient("localhost", 10006, "/index.html");
+        assertEquals("text/html", client.getResponseHeader("Content-Type"));
+    }
+
+    @Test
+    void shouldReturn404IfFileNotFound() throws IOException {
+        HttpServer server = new HttpServer(10007, dataSource);
+        HttpClient client = new HttpClient("localhost", 10007, "/notFound.txt");
+        assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldPostNewMember() throws IOException, SQLException {
+        HttpServer server = new HttpServer(10008, dataSource);
+        HttpClient client = new HttpClient("localhost", 10008, "/members", "POST", "first_name=Christian");
+        assertEquals(200, client.getStatusCode());
+        assertThat(server.getMembers())
+                .extracting(member -> member.getFirstName())
+                .contains("Christian");
+    }
+    @Test
+    void shouldReturnExistingMembers() throws IOException, SQLException {
+        HttpServer server = new HttpServer(10009, dataSource);
+        MemberDao memberDao = new MemberDao(dataSource);
+        Member member = new Member();
+        member.setFirstName("Christian");
+        member.setLastName("Lie");
+        member.setEmail("chris@egms.no");
+        memberDao.insert(member);
+        HttpClient client = new HttpClient("localhost", 10009, "/members");
+        assertThat(client.getResponseBody()).contains("<li>" + "Christian Lie, chris@egms.no</li>");
+
+    }
+
+}
